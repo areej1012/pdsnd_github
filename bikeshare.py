bc43d1094035511ffc6fb13e0778d9732bb3d62e
areej <areej101223@gmail.com>
2021-08-07 17:06:13 +0300

    delete a  word in list CITIES

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-CITIES=  ['chicago', 'new york city','new york', 'washington']
+CITIES=  ['chicago', 'new york city', 'washington']
 months = ['january', 'february', 'march', 'april', 'may', 'june']
 def get_filters():
     """
